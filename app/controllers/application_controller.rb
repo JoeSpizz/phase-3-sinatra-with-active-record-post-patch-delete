0e08fb8ffd231372ebd02714fe3ad524c25eecab
Joe <spizjo01@gmail.com>
2022-09-26 10:19:57 -0600

    commit tree 3ddcf3686ee0e16a92359e365d6788687c216a48

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-
+  get '/' do
+    "Why wasn't this built by default?"
+  end
   get '/games' do
     games = Game.all.order(:title).limit(10)
     games.to_json
@@ -14,6 +16,33 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+      end
+
+    delete '/reviews/:id' do
+      review = Review.find(params[:id])
+      review.destroy
+      review.to_json
   end
+  
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
 
 end
